5b4ae8253b6cba24568b09417b13e7f139af19c1
shubhamjeurkar <shubhamjeurkar11@gmail.com>
2025-08-31 23:11:02 +0530

    feat: add Swagger documentation and implement rate limiting, helmet, and CORS middleware

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimiter = require('express-rate-limit');
 
 const connectDB = require('./db/connect');
 
@@ -12,16 +15,27 @@ const noteRouter = require('./routes/note');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticateUser = require('./middleware/authentication');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
-
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+ windowMs: 15 * 60 * 1000, //15 minutes
+ max: 100 // limit each IP to 100 requests per windowMS
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/notes', authenticateUser, noteRouter);
 
 app.get('/', (req, res) => {
- res.send('Notes API')
+ res.status(200).redirect('/api-docs');
 })
 const port = process.env.PORT || 5000;
 
